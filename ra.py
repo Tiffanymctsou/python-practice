1acda06698a5d7739de4dd5ecdea40458d1b5f76
Tiffany Tsou <tiffany.mctsou@gmail.com>
2018-11-08 21:35:38 +0800

    add filtering

--- ra.py
+++ ra.py
@@ -11,4 +11,10 @@ print('檔案讀取完了, 總共有', len(data), '筆資料')
 sum_len = 0
 for d in data:
 	sum_len += len(d) # sum_len = sum_len + len(d)
-	print('每筆留言的平均長度是', sum_len/len(data), '個字')
+print('每筆留言的平均長度是', sum_len/len(data), '個字')
+
+new = []
+for d in data: # d是每一個留言(字串); data是清單
+	if len(d) < 100:
+		new.append(d)
+print('一共有', len(new), '留言長度小於100個字')
